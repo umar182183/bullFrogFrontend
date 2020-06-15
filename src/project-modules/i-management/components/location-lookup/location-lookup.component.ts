8e0da4ca33531586c5468c5bf767606ff75d0726
muhammad_umar_jaffar <omar@jjtestsite.us>
2020-06-15 18:12:01 +0500

    updates

--- src/project-modules/i-management/components/location-lookup/location-lookup.component.ts
+++ src/project-modules/i-management/components/location-lookup/location-lookup.component.ts
@@ -60,7 +60,42 @@ export class LocationLookupComponent implements OnInit {
     }, {
       letter: 'I',
       names: ['Idaho', 'Illinois', 'Indiana', 'Iowa']
-    },{
+    }, {
+      letter: 'K',
+      names: ['Kansas', 'Kentucky']
+    }, {
+      letter: 'L',
+      names: ['Louisiana']
+    }, {
+      letter: 'M',
+      names: ['Maine', 'Maryland', 'Massachusetts', 'Michigan',
+        'Minnesota', 'Mississippi', 'Missouri', 'Montana']
+    }, {
+      letter: 'N',
+      names: ['Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+        'New Mexico', 'New York', 'North Carolina', 'North Dakota']
+    }, {
+      letter: 'O',
+      names: ['Ohio', 'Oklahoma', 'Oregon']
+    }, {
+      letter: 'P',
+      names: ['Pennsylvania']
+    }, {
+      letter: 'R',
+      names: ['Rhode Island']
+    }, {
+      letter: 'S',
+      names: ['South Carolina', 'South Dakota']
+    }, {
+      letter: 'T',
+      names: ['Tennessee', 'Texas']
+    }, {
+      letter: 'U',
+      names: ['Utah']
+    }, {
+      letter: 'V',
+      names: ['Vermont', 'Virginia']
+    }, {
       letter: 'W',
       names: ['Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
     }];
