eba2683a5dea5c0d917f7ec21249b16e12251efc
Muhammad_Umar_Jaffar <omar@jjtestsite.us>
2020-07-29 17:38:10 +0500

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,18 @@
 
-export const environment = {
-  production: false,
-  baseUrl :"http://testretail.bullfrogspas.com/",
-  apiBaseUrl: "http://10.0.3.40/AspCoreApiIIS/api/"
-}
+let env = "APP"; //in case of localhost
+
+const relativePaths = {
+  APP: {
+    production: false,
+    baseUrl :"http://testretail.bullfrogspas.com/",
+    apiBaseUrl: "http://10.0.3.40/AspCoreApiIIS/api/"
+  },
+  Q2: {
+    production: false,
+    baseUrl :"http://testretail.bullfrogspas.com/",
+    apiBaseUrl: "localhost:6200/api/"
+  },
+  
+};
+
+export const environment = relativePaths[env];
